687e806a2361c09053e14467d8567bbd8eb3eb44
arrokhman6 <93060024+arrokhman6@users.noreply.github.com>
2021-11-05 19:44:54 +0700

    IntentFatkhur
    belajar membuat intent tp gk bisa di running padahal kodingan nya benar dan agak lemot karena ram 2 gb

---
+++ app/src/main/java/com/fatkhur/intentfatkhur/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.fatkhur.intentfatkhur
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+    private lateinit var btnMoveActivity: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnMoveActivity = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+    override fun onClick(v:View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.btn_move_activity -> run {
+                    val intent = Intent(this, btnMoveActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+}
